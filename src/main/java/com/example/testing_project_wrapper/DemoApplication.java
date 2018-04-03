d9b09e50363f6dba597cfe0a3a4368880c9734fe
mohammad <mohammadhunan@gmail.com>
2018-04-03 10:20:26 -0400

    created springboot wrapper project

---
+++ src/main/java/com/example/testing_project_wrapper/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.testing_project_wrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
